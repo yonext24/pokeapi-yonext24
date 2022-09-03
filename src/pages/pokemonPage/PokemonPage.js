881b6fac0313da68ac8c7f3745ffc5b3d36b6e95
Yanzz <yonielkpo@gmail.com>
2022-09-03 00:56:35 -0300

    .

--- src/pages/pokemonPage/PokemonPage.js
+++ src/pages/pokemonPage/PokemonPage.js
@@ -1,18 +1,14 @@
 import { useParams } from 'react-router-dom'
 import { getPokemons } from '../../services/getPokemons'
 import { useEffect, useState } from 'react'
-import Error404 from '../error404/Error404'
 
 export default function PokemonPage () {
-  const [loading, setLoading] = useState(false)
   const [pokemonData, setPokemonData] = useState(null)
   const { pokemon } = useParams()
 
   useEffect(() => {
-    setLoading(true)
     getPokemons(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     .then(res => setPokemonData(res))
-    .then(setLoading(false))
   }, [pokemon]) 
 
   if (pokemonData === null || pokemonData.ok === false) {
